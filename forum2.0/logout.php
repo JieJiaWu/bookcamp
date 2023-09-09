67e7c8620faacb99517da1144568e052e311d27d
JiaJieWu <wujia61819@gmail.com>
2023-09-09 13:58:09 +0800

    Removed forum2.0 from index

--- forum2.0/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // 確保啟用 SESSION 功能
-if (isset($_GET["logout"]) && ($_GET["logout"] == "true")) {
-    unset($_SESSION["username"]);
-    unset($_SESSION["id"]);
-    header("Location:login.php");
-    // echo "登出成功";
-}
-?>
